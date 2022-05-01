20fa98126563b115141eb01c23fbe6764cbe2528
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-01 19:58:16 -0300

    Add array

---
+++ Array/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+namespace Array
+{
+  public class Program
+  {
+    static void TrocarNome(string nome, string nomeNovo)
+    {
+      nome = nomeNovo;
+    }
+
+    static void MudarParaImpar(int[] pares)
+    {
+      for (int i = 0; i < pares.Length; i++)
+      {
+        pares[i] = pares[i] + 1;
+      }
+    }
+    public static void Main()
+    {
+      int[] pares = new int[] { 0, 2, 4, 6, 8 };
+
+      MudarParaImpar(pares);
+      Console.WriteLine($"Os impares {string.Join(",", pares)}");
+
+
+    }
+  }
+}
