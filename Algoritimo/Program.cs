139143b97970d36bd2ac2f2766ca16797057a866
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-02 18:49:47 -0300

    Algoritimo first

---
+++ Algoritimo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algoritimo
+{
+
+  public class Program
+  {
+    static int EncontrarNumero(int[] numeros, int numero)
+    {
+      for (int i = 0; i < numeros.Length; i++)
+      {
+        if (numeros[i] == numero)
+          return i;
+      }
+      return -1;
+    }
+
+    public static void Main()
+    {
+      int[] numeros = new int[] { 0, 2, 4, 6, 8 };
+      Console.WriteLine($"Digite o numero que gostaria de encontrar");
+      var numero = int.Parse(Console.ReadLine());
+      var idxEncontrado = EncontrarNumero(numeros, numero);
+
+      if (idxEncontrado >= 0)
+      {
+
+        Console.WriteLine($"O numero digitado esta na posição {idxEncontrado}");
+      }
+      else
+      {
+        Console.WriteLine("O numero digitado nao foi encontrado");
+      }
+    }
+  }
+}
