464568bf45c5e24ced830462cb95089f4f1abe27
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-05 21:07:26 -0300

    Utilizando Struct adicionado

---
+++ UtilizandoStruct/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace AtribuirValor
+{
+  public class Program
+  {
+    static bool EncontrarPessoa(List<StructPessoa> pessoas, StructPessoa pessoa)
+    {
+      foreach (var item in pessoas)
+      {
+        if (item.Equals(pessoa))
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+    public static void Main()
+    {
+      List<StructPessoa> pessoas = new List<StructPessoa>()
+      {
+        new StructPessoa(){Nome = "Ricardo"},
+        new StructPessoa(){Nome = "José"},
+        new StructPessoa(){Nome = "Maria"},
+        new StructPessoa(){Nome = "Fernando"},
+        new StructPessoa(){Nome = "Marcos"},
+      };
+
+      Console.WriteLine("Digite a pessoa que gostaria de localizar");
+      var nome = Console.ReadLine();
+      var pessoa = new StructPessoa() { Nome = nome };
+      var encontrado = EncontrarPessoa(pessoas, pessoa);
+      if (encontrado)
+      {
+        Console.WriteLine("Pessoa localizada!");
+      }
+      else
+      {
+        Console.WriteLine("Pessoa nao localizada");
+      }
+    }
+  }
+}
