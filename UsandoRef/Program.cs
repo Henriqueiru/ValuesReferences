349b8a392910b16dd23d5976393aa1a65de6e67c
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-05 21:21:16 -0300

    Referenciando Values usando ref

---
+++ UsandoRef/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ValueTypes
+{
+  public class Program
+  {
+    static void Adicionar20(ref int a)
+    {
+      a += 20;
+    }
+    static void Main()
+    {
+      int a = 2;
+      Adicionar20(ref a);
+      Console.WriteLine($"O valor da variavel a é: {a} ");
+    }
+  }
+}
